e95be537758e9e4c87b7c6a65ea8f744965c9f0b
lelethecode <quangle2072008@gmail.com>
2024-09-23 17:03:14 +0700

    Update SlideBar.jsx

--- src/app/components/SlideBar.jsx
+++ src/app/components/SlideBar.jsx
@@ -40,33 +40,34 @@ const SlideBar = () => {
         <img src="/images/logo.png" className="faslogo" id="ex-mar" alt="Logo" />
         <h2 className="logotitle">FAS</h2>
         <span className="divider" id="ex-mar"></span>
-        
-        <div className="navbar">
-          <button
-            className="hamburger"
-            onClick={toggleMenu}
-            aria-expanded={menuOpen}
-            aria-controls="button-bar"
-          >
-            ☰
-          </button>
-          <div className={`button-bar ${menuOpen ? 'open' : ''}`} id="button-bar">
-            {user ? (
-              <>
-                <div className="user-info">
-                  <div className="welcome1"><p>Welcome, {user.name}</p></div> {/* Adjust based on your user data structure */}
-                  <button className="buttons" onClick={handleLogout}>Logout</button>
-                </div>
-              </>
-            ) : (
-              <>
-                <button className="buttons" onClick={handleLogin}>Đăng nhập</button>
-                <button className="buttons" onClick={handleRegister}>Đăng ký</button>
-              </>
-            )}
-            <button className="buttons" onClick={handleFoodSelection}>Chọn Món</button>
-            <button className="buttons" onClick={handleSelectedFood}>Món ăn bạn đã chọn</button>
-            <button className="buttons" onClick={home}>Trang Chủ</button>
+        <div className="navcontain">
+          <div className="navbar">
+            <button
+              className="hamburger"
+              onClick={toggleMenu}
+              aria-expanded={menuOpen}
+              aria-controls="button-bar"
+            >
+              ☰
+            </button>
+            <div className={`button-bar ${menuOpen ? 'open' : ''}`} id="button-bar">
+              {user ? (
+                <>
+                  <div className="user-info">
+                    <div className="welcome1"><p>Welcome,</p><p>{user.name}</p>></div> {/* Adjust based on your user data structure */}
+                    <button className="buttons" onClick={handleLogout}>Logout</button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <button className="buttons" onClick={handleLogin}>Đăng nhập</button>
+                  <button className="buttons" onClick={handleRegister}>Đăng ký</button>
+                </>
+              )}
+              <button className="buttons" onClick={handleFoodSelection}>Chọn Món</button>
+              <button className="buttons" onClick={handleSelectedFood}>Món ăn bạn đã chọn</button>
+              <button className="buttons" onClick={home}>Trang Chủ</button>
+            </div>
           </div>
         </div>
       </div>
