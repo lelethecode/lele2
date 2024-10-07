ad2a4d32fdbd85c18eb53e52ebffec5b6646991b
Sin-Pham <phamnguyenkhai27@gmail.com>
2024-10-07 17:25:36 +0700

    hehehehe

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -3,7 +3,6 @@ export default function Home() {
     return (
       <div className="hello-txt">
         <h1 className="chao">Chào mừng đến với trang chính!</h1>
-        <p className="chao">Nội dung trang chính của bạn sẽ hiển thị ở đây.</p>
       </div>
     );
   }
